f2e6e29e2f8f7d06040bfb8efed64000cd27c1bc
MaheshBhadane <mahesh.bhadane@torinit.ca>
2022-07-29 11:13:55 +0530

    Add,Remove,Priority Functionalities

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
 
-const ToDoApp = () => {
-    return(
-        <>
-        
-        </>
-    )
-}
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  });
 
-export default ToDoApp;
+  const submitUpdate = value => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: ''
+    });
+  };
+
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className='icons'
+          onClick={() => removeTodo(todo.id)}
+        />
+        < div onClick={() => setEdit({ id: todo.id, value: todo.text })}
+        />
+      </div>
+    
+  ));
+};
+
+export default Todo;
